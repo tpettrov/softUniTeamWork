edf1897706d5f8d150b2ba47b54e5df85b236e35
rtalev <rtalev@gmail.com>
2016-08-28 19:39:09 +0300

    Added posts table in database

---
+++ BlogSystem/BlogSystem/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogSystem.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        public int Author { get; set; }
+    }
+}
