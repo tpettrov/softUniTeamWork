78525972bb65d04329ed142855c709428610c493
rtalev <rtalev@gmail.com>
2016-08-29 15:31:56 +0300

    Added PostsController with CRUD operations, Post views for posts and automatic migrations. Deleted About.cshtml view.

--- BlogSystem/BlogSystem/Models/AccountViewModels.cs
+++ BlogSystem/BlogSystem/Models/AccountViewModels.cs
@@ -79,6 +79,10 @@ namespace BlogSystem.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "FullName")]
+        public string FullName { get; set; }
     }
 
     public class ResetPasswordViewModel
--- BlogSystem/BlogSystem/Models/Post.cs
+++ BlogSystem/BlogSystem/Models/Post.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,16 +9,32 @@ namespace BlogSystem.Models
 {
     public class Post
     {
+        public Post()
+        {
+            this.Date = DateTime.Now;
+        }
+
         [Key]
         public int Id { get; set; }
 
         [Required]
         [StringLength(200)]
+        [Display(Name = "Заглавие")]
         public string Title { get; set; }
 
         [Required]
+        [Display(Name = "Текст")]
+        [DataType(DataType.MultilineText)]
         public string Body { get; set; }
 
-        public int Author { get; set; }
+        [Display(Name = "Автор")]
+        [ForeignKey("Author_Id")]
+        public ApplicationUser Author { get; set; }
+        
+        public string Author_Id { get; set; }
+
+        [Required]
+        [Display(Name = "Дата")]
+        public DateTime Date { get; set; }
     }
 }
