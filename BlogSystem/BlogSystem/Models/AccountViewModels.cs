a458bc561840cbc5567c524de2d768f0d86ad019
rtalev <rtalev@gmail.com>
2016-08-29 16:35:21 +0300

    Fixed navigation bar

--- BlogSystem/BlogSystem/Models/AccountViewModels.cs
+++ BlogSystem/BlogSystem/Models/AccountViewModels.cs
@@ -5,7 +5,7 @@ namespace BlogSystem.Models
 {
     public class ExternalLoginConfirmationViewModel
     {
-        [Required]
+        
         [Display(Name = "Email")]
         public string Email { get; set; }
     }
